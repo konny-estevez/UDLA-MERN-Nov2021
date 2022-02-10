10e476e8167b34583e2c69aee3cf7ed44d3ce7de
cheojorge <cheojorge@hotmail.com>
2022-02-09 22:14:50 -0500

    Co-authored-by: Javier_Vilegas <javillegasna@users.noreply.github.com>

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,16 +1,40 @@
-const Details = () => {
-    return (
-    <>
-        <div className="card" styles={{"width": "18rem"}}>
-         <img src="..." className="card-img-top" alt="..."/>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-        <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
-    </>
-        );
+const Details = () => {
+    const [datoPoke, setDatoPoke] = useState()
+    let url = "";
+    const { id } = useParams();
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(res => setDatoPoke(res.data))
+    }, []);
+    if(datoPoke){
+        url = datoPoke.sprites.front_default
+        console.log(url)
+    }
+    console.log(datoPoke)
+    return (
+        <>{ datoPoke &&
+            <div className="card" styles={{ "width": "18rem" }}>
+                <img  src={url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{datoPoke.name}</h5>
+                    <p className="card-text">Height: {datoPoke.height}</p>
+                    <p className="card-text">Weight: {datoPoke.weight}</p>
+                    <p className="card-text">Base Experience: {datoPoke.base_experience}</p>
+                    <Link to={"/"}>
+                        <button className="btn btn-primary">
+                            HOME
+                        </button>
+                    </Link>
+
+
+                </div>
+            </div>
+            }
+        </>
+    );
 }
 export default Details;
